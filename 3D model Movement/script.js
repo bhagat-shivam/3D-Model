1caa42995b6282acb24bf431573e1e5df9c72b05
shivam kumar bhagat <54414190+bhagat-shivam@users.noreply.github.com>
2023-08-07 06:25:28 +0530

    1st commit

---
+++ 3D model Movement/script.js
@@ -0,0 +1,12 @@
+
+var modelViewer = document.getElementById('model');
+
+function stop(){
+    modelViewer.animationName = 'none';
+}
+function run(){
+    modelViewer.animationName = 'Running';
+}
+function wave(){
+    modelViewer.animationName = 'Wave';
+}
